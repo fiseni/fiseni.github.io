e284ffbefe38dbd4414f4f8ad5d4a37d8a82ca1f
Fati Iseni <fati.iseni@pozitrongroup.com>
2024-11-13 00:28:50 +0100

    Bust css cache

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,1 +1,1 @@
-const swconf = { cacheName: 'chirpy-1731452246',resources: [ '/assets/css/jekyll-theme-chirpy.css', '/', '/categories/', '/tags/', '/archives/', ], interceptor: {paths: [ ],urlPrefixes: [ ] }, purge: false };
+const swconf = { cacheName: 'chirpy-1731452246',resources: [ '/assets/css/jekyll-theme-chirpy-poz.css', '/', '/categories/', '/tags/', '/archives/', ], interceptor: {paths: [ ],urlPrefixes: [ ] }, purge: false };
